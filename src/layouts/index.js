baa9887055b080f12aaaf578a54cf712fde57fec
李雨健 <icanflyanywhere@126.com>
2026-01-05 16:48:32 +0800

    暗黑模式添加

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import { Image, Watermark, ConfigProvider } from "antd";
+import React, { useState } from 'react';
+import { Image, Watermark, ConfigProvider, theme } from "antd";
 import { useNavigate, useRouteProps, Outlet, connect } from 'umi';
 import WinBar from './WinBar';
 import SideBar from './SideBar';
 import zhCN from 'antd/locale/zh_CN';
 import "./index.less"
 
-const theme= {
-  components: {
-    Alert: {
-      withDescriptionPadding: [10, 12]
-    },
-  },
-}
-
 const Layout = (props) => {
   const routeProps = useRouteProps()
   const { name } = routeProps;
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <div className="layout">
-      <WinBar />
-      <div className="app-content">
-        <ConfigProvider  locale={zhCN} theme={theme}>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        components: { Alert: { withDescriptionPadding: [10, 12] } },
+        algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm
+      }}>
+      <div className="layout">
+        <WinBar />
+        <div className="app-content">
           <Outlet/>
-        </ConfigProvider>
+        </div>
       </div>
-    </div>
+    </ConfigProvider>
   );
 }
 
